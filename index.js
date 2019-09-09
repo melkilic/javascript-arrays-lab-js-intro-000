487a2e2476699fe6812430b7dd852d0c1180b27d
Melike KILIC <melike180400@gmail.com>
2019-09-09 17:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"];//define your array here
 function destructivelyAppendKitten(kittens){
-  var kittens=["Ralph"];
-  kittens.concat(kittens);
-}
+  kittens.push("Ralph");
+  }
 // Add your functions and code here
